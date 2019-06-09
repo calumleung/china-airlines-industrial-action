c3ec7204416ed6d82d19bd6073b9d3cac4eeee11
ihsuanlin1997 <50326223+ihsuanlin1997@users.noreply.github.com>
2019-06-09 20:21:37 +0800

    更新～聊天室

--- chatbox/chatbox.js
+++ chatbox/chatbox.js
@@ -1,7 +1,12 @@
 // JavaScript Document
-// function start(){
-// document.getElementById("chat1").style.display = "block";
-// }
+
+
+function join(){
+  document.getElementById("chatbox_start").style.display = "none";
+  document.getElementById("chatbox_footer0").style.display = "none";
+  document.getElementById("chatbox_footer1").style.display = "block";
+} 
+
 
 function add1() {
   document.getElementById("chat1").style.margin = "0%";
@@ -14,14 +19,14 @@ function add1() {
     items[count].style.display = "block";
     count++;
     console.log(count);
-    var chatbox = document.getElementsByClassName("chatbox_body")[0];
-    chatbox.scrollTop = chatbox.scrollHeight;
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
     if (count == items.length) {
       document.getElementById("chatbox_footer1").style.display = "none";
       document.getElementById("chatbox_footer2").style.display = "block";
       clearInterval(timeId);
     }
-  }, 1500);
+  }, 500);
 }
 
 function add2() {
@@ -35,14 +40,14 @@ function add2() {
     items[count].style.display = "block";
     count++;
     console.log(count);
-    var chatbox = document.getElementsByClassName("chatbox_body")[0];
-    chatbox.scrollTop = chatbox.scrollHeight;
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
     if (count == items.length) {
       document.getElementById("chatbox_footer2").style.display = "none";
       document.getElementById("chatbox_footer3").style.display = "block";
       clearInterval(timeId);
     }
-  }, 3000);
+  }, 500);
 }
 
 function add3() {
@@ -56,20 +61,33 @@ function add3() {
     items[count].style.display = "block";
     count++;
     console.log(count);
-    var chatbox = document.getElementsByClassName("chatbox_body")[0];
-    chatbox.scrollTop = chatbox.scrollHeight;
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
     if (count == items.length) {
       document.getElementById("chatbox_footer3").style.display = "none";
       document.getElementById("chatbox_footer4").style.display = "block";
+      if(window.screen.width < 576){
+        document.getElementById("chatbox_word41").style.display = "block";
+      }
+      else{
+        document.getElementById("chatbox_word41").style.display = "none";
+        document.getElementById("chatbox_word4").style.display = "block";
+      }
       clearInterval(timeId);
     }
-  }, 3000);
+  }, 500);
 }
 
 function add4() {
   document.getElementById("chat4").style.marginRight = "0%";
   document.getElementById("chat4").style.display = "block";
-  document.getElementById("chatbox_word4").style.display = "none";
+  if(window.screen.width < 576){
+    document.getElementById("chatbox_word41").style.display = "none";
+  }
+  else{
+    document.getElementById("chatbox_word4").style.display = "none";
+  }
+  
   var items = document.querySelectorAll(".chatbox_bubble_container4");
   var count = 0;
 
@@ -77,20 +95,32 @@ function add4() {
     items[count].style.display = "block";
     count++;
     console.log(count);
-    var chatbox = document.getElementsByClassName("chatbox_body")[0];
-    chatbox.scrollTop = chatbox.scrollHeight;
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
     if (count == items.length) {
       document.getElementById("chatbox_footer4").style.display = "none";
       document.getElementById("chatbox_footer5").style.display = "block";
+      if(window.screen.width < 576){
+        document.getElementById("chatbox_word51").style.display = "block";
+      }
+      else{
+        document.getElementById("chatbox_word51").style.display = "none";
+        document.getElementById("chatbox_word5").style.display = "block";
+      }
       clearInterval(timeId);
     }
-  }, 3000);
+  }, 500);
 }
 
 function add5() {
   document.getElementById("chat5").style.marginRight = "0%";
   document.getElementById("chat5").style.display = "block";
-  document.getElementById("chatbox_word5").style.display = "none";
+  if(window.screen.width < 576){
+    document.getElementById("chatbox_word51").style.display = "none";
+  }
+  else{
+    document.getElementById("chatbox_word5").style.display = "none";
+  }
   var items = document.querySelectorAll(".chatbox_bubble_container5");
   var count = 0;
 
@@ -98,32 +128,82 @@ function add5() {
     items[count].style.display = "block";
     count++;
     console.log(count);
-    var chatbox = document.getElementsByClassName("chatbox_body")[0];
-    chatbox.scrollTop = chatbox.scrollHeight;
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
     if (count == items.length) {
       document.getElementById("chatbox_footer5").style.display = "none";
       document.getElementById("chatbox_footer6").style.display = "block";
       clearInterval(timeId);
     }
-  }, 3000);
+  }, 500);
 }
 
 function add6() {
-  document.getElementById("chat6").style.marginRight = "0%";
+  document.getElementById("chatbox_footer6").style.display = "none";
+  document.getElementById("chatbox_footer7").style.display = "block";
   document.getElementById("chat6").style.display = "block";
-  document.getElementById("chatbox_word5").style.display = "none";
-  var items = document.querySelectorAll(".chatbox_bubble_container6");
+}
+
+function add7() {
+  // document.getElementById("chatbox_footer7").style.background = "#00AAAA";
+  document.getElementById("chat_end1").style.display = "none";
+  document.getElementById("chat_end2").style.display = "none";
+  document.getElementById("chat_end3").style.display = "none";
+  document.getElementById("chat7").style.display = "block";
+  var items = document.querySelectorAll(".chatbox_bubble_container7");
   var count = 0;
 
   var timeId = setInterval(function() {
     items[count].style.display = "block";
     count++;
     console.log(count);
-    var chatbox = document.getElementsByClassName("chatbox_body")[0];
-    chatbox.scrollTop = chatbox.scrollHeight;
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
     if (count == items.length) {
-      document.getElementById("chatbox_word6").style.display = "none";
       clearInterval(timeId);
     }
-  }, 3000);
+  }, 500);
+}
+
+
+function add8() {
+  // document.getElementById("chatbox_footer7").style.background = "#00AAAA";
+  document.getElementById("chat_end1").style.display = "none";
+  document.getElementById("chat_end2").style.display = "none";
+  document.getElementById("chat_end3").style.display = "none";
+  document.getElementById("chat8").style.display = "block";
+  var items = document.querySelectorAll(".chatbox_bubble_container8");
+  var count = 0;
+
+  var timeId = setInterval(function() {
+    items[count].style.display = "block";
+    count++;
+    console.log(count);
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
+    if (count == items.length) {
+      clearInterval(timeId);
+    }
+  }, 500);
+}
+
+function add9() {
+  // document.getElementById("chatbox_footer7").style.background = "#00AAAA";
+  document.getElementById("chat_end1").style.display = "none";
+  document.getElementById("chat_end2").style.display = "none";
+  document.getElementById("chat_end3").style.display = "none";
+  document.getElementById("chat9").style.display = "block";
+  var items = document.querySelectorAll(".chatbox_bubble_container9");
+  var count = 0;
+
+  var timeId = setInterval(function() {
+    items[count].style.display = "block";
+    count++;
+    console.log(count);
+    // var chatbox = document.getElementsByClassName("chatbox_body")[0];
+    // chatbox.scrollTop = chatbox.scrollHeight;
+    if (count == items.length) {
+      clearInterval(timeId);
+    }
+  }, 500);
 }
