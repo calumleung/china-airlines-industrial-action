2927c81bf325bfa34a442ff0f7836ed97c40c821
calumleung <calumleung@gmail.com>
2019-06-19 05:10:41 +0800

    updated

--- plane-game/js/action.js
+++ plane-game/js/action.js
@@ -20,30 +20,30 @@ function lowscore() {
   document.getElementById("score").innerHTML=score;
     document.getElementById("airplaneimg").style.transform = "rotate(-15deg)";
 if(score==11000){
-    document.getElementById("m1").style.display = "block";
+    document.getElementById("m1").style.display = "inline-table";
     document.getElementById("m2").style.display = "none";
 }
 else if(score>=10800){
-    document.getElementById("m2").style.display = "block";
+    document.getElementById("m2").style.display = "inline-table";
     document.getElementById("m3").style.display = "none";
 }
 else if(score>=10600){
-    document.getElementById("m3").style.display = "block";
+    document.getElementById("m3").style.display = "inline-table";
     document.getElementById("m4").style.display = "none";
 }
 else if(score>=10400){
-    document.getElementById("m4").style.display = "block";
+    document.getElementById("m4").style.display = "inline-table";
     document.getElementById("m5").style.display = "none";
 }
 else if(score>=10200){
-    document.getElementById("m5").style.display = "block";
+    document.getElementById("m5").style.display = "inline-table";
     document.getElementById("m6").style.display = "none";
 }
 else if(score>=10000){
-    document.getElementById("m6").style.display = "block";
+    document.getElementById("m6").style.display = "inline-table";
 }
 else if(score>=8000){
-    document.getElementById("m7").style.display = "block";
+    document.getElementById("m7").style.display = "inline-table";
     document.getElementById("m6").style.display = "none";
     document.getElementById("m5").style.display = "none";
   document.getElementById("m4").style.display = "none";
@@ -52,19 +52,19 @@ else if(score>=8000){
   document.getElementById("m1").style.display = "none";
 }
 else if(score>=6000){
-    document.getElementById("m8").style.display = "block";
+    document.getElementById("m8").style.display = "inline-table";
     document.getElementById("m7").style.display = "none";
 }
 else if(score>=4000){
-    document.getElementById("m9").style.display = "block";
+    document.getElementById("m9").style.display = "inline-table";
     document.getElementById("m8").style.display = "none";
 }
 else if(score>=2000){
-    document.getElementById("m10").style.display = "block";
+    document.getElementById("m10").style.display = "inline-table";
     document.getElementById("m9").style.display = "none";
 }
 else {
-    document.getElementById("m11").style.display = "block";
+    document.getElementById("m11").style.display = "inline-table";
     document.getElementById("m10").style.display = "none";
 }
 }
@@ -84,30 +84,30 @@ function highscore() {
       }
   }
 if(score==11000){
-    document.getElementById("m1").style.display = "block";
+    document.getElementById("m1").style.display = "inline-table";
     document.getElementById("m2").style.display = "none";
 }
 else if(score>=10800){
-    document.getElementById("m2").style.display = "block";
+    document.getElementById("m2").style.display = "inline-table";
     document.getElementById("m3").style.display = "none";
 }
 else if(score>=10600){
-    document.getElementById("m3").style.display = "block";
+    document.getElementById("m3").style.display = "inline-table";
     document.getElementById("m4").style.display = "none";
 }
 else if(score>=10400){
-    document.getElementById("m4").style.display = "block";
+    document.getElementById("m4").style.display = "inline-table";
     document.getElementById("m5").style.display = "none";
 }
 else if(score>=10200){
-    document.getElementById("m5").style.display = "block";
+    document.getElementById("m5").style.display = "inline-table";
     document.getElementById("m6").style.display = "none";
 }
 else if(score>=10000){
-    document.getElementById("m6").style.display = "block";
+    document.getElementById("m6").style.display = "inline-table";
 }
 else if(score>=8000){
-    document.getElementById("m7").style.display = "block";
+    document.getElementById("m7").style.display = "inline-table";
     document.getElementById("m6").style.display = "none";
     document.getElementById("m5").style.display = "none";
   document.getElementById("m4").style.display = "none";
@@ -116,19 +116,19 @@ else if(score>=8000){
   document.getElementById("m1").style.display = "none";
 }
 else if(score>=6000){
-    document.getElementById("m8").style.display = "block";
+    document.getElementById("m8").style.display = "inline-table";
     document.getElementById("m7").style.display = "none";
 }
 else if(score>=4000){
-    document.getElementById("m9").style.display = "block";
+    document.getElementById("m9").style.display = "inline-table";
     document.getElementById("m8").style.display = "none";
 }
 else if(score>=2000){
-    document.getElementById("m10").style.display = "block";
+    document.getElementById("m10").style.display = "inline-table";
     document.getElementById("m9").style.display = "none";
 }
 else {
-    document.getElementById("m11").style.display = "block";
+    document.getElementById("m11").style.display = "inline-table";
     document.getElementById("m10").style.display = "none";
 }
 }
